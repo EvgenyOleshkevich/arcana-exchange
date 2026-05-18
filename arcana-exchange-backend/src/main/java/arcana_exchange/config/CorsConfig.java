82ade817833664db4b4b73beff104e5c6cf0fa15
Evgeny <oleshkevich19990802@mail.ru>
2026-05-19 00:14:02 +0200

    fix cors

--- arcana-exchange-backend/src/main/java/arcana_exchange/config/CorsConfig.java
+++ arcana-exchange-backend/src/main/java/arcana_exchange/config/CorsConfig.java
@@ -1,4 +1,27 @@
 package arcana_exchange.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "https://arcana-exchange.vercel.app",
+                                "https://arcana-exchange.info"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
